1eecee16c58e2f0798fbb4b273bbdb695ff81ecb
bittib010 <skaftunen@gmail.com>
2024-09-17 10:35:06 +0200

    first template backend

--- min_setup/server/controllers/user.controller.js
+++ min_setup/server/controllers/user.controller.js
@@ -1,48 +1,54 @@
 import { db } from '../libs/dbConnect.js';
 import { ObjectId } from 'mongodb';
+import bcrypt from 'bcrypt';
 
 const collection = db.collection('users');
 
-export const test = async (req, res) => {
-    let results = await collection.indexInformation({}).toArray();
-    res.status(200).json(results);
-}
-
 export const getUser = async (req, res, next) => {
-    try {
-        const query = { _id: new ObjectId(req.params.id) }
-        const user = await collection.findOne(query)
+  try {
+    const query = { _id: new ObjectId(req.params.id) };
+    const user = await collection.findOne(query);
 
-        if (!user) {
-
-            return next({ status: 404, message: 'User not found' })
-        }
-        res.status(200).json(user)
-
-    } catch (err) {
-        next({ status: 500, err })
+    if (!user) {
+      return next({ status: 404, message: 'User not found!' });
     }
-}
+
+    res.status(200).json(user);
+  } catch (error) {
+    next({ status: 500, error });
+  }
+};
 
 export const updateUser = async (req, res, next) => {
-    try {
-        if (req.body.password) { // if body has password
-            req.body.passwod = await bcrypt.hash(req.body.password, 10);
-        }
-        const query = { _id: new ObjectId(req.params.id) }
-        const data = {
-            $set: {
-                ...req.body, updatedAt: new Date().toISOString(),
-
-            },
-        }
-        const options = {
-            returnDocument: 'after',
-        }
-
-        const updatedUser = await collection.findOneAndUpdate(query, data, options)
-        const { password: pass, updatedAt, createdAt, ...rest } = updatedUserres.status(200).json(updatedUser)
-    } catch (err) {
-        next({ status: 500, err })
+  try {
+    if (req.body.password) {
+      req.body.password = await bcrypt.hash(req.body.password, 10);
     }
-}
+    const query = { _id: new ObjectId(req.params.id) };
+    const data = {
+      $set: {
+        ...req.body,
+        updatedAt: new Date().toISOString(),
+      },
+    };
+    const options = {
+      returnDocument: 'after',
+    };
+
+    const updatedUser = await collection.findOneAndUpdate(query, data, options);
+    const { password: pass, updatedAt, createdAt, ...rest } = updatedUser;
+    res.status(200).json(rest);
+  } catch (error) {
+    next({ status: 500, error });
+  }
+};
+
+export const deleteUser = async (req, res, next) => {
+  try {
+    const query = { _id: new ObjectId(req.params.id) };
+    await collection.deleteOne(query);
+    res.status(200).json({ message: 'User has been deleted!' });
+  } catch (error) {
+    next({ status: 500, error });
+  }
+};
--- min_setup/server/server.js
+++ min_setup/server/server.js
@@ -1,18 +1,22 @@
 import express from 'express';
 import 'dotenv/config';
-import { db } from './libs/dbConnect.js';
-import userRouter from './routes/user.route.js'
+
+import userRouter from './routes/user.route.js';
+import { errorHandler } from './libs/middleware.js';
 
 const PORT = 8000;
 const app = express();
 
-app.use('/api/v1/users', userRouter)
+app.use(express.json());
+
+app.use('/api/v1/users', userRouter);
 
 app.use('*', (req, res) => {
   res.status(404).json({ message: 'not found' });
 });
 
-// Start the Express server
+app.use(errorHandler);
+
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
